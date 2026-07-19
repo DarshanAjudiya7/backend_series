bdb91d802c8ed83f0ae8d57136378bdbce02c32f
DarshanAjudiya7 <185569366+DarshanAjudiya7@users.noreply.github.com>
2026-07-19 16:20:29 +0530

    remove wrong content from the file app.js

--- app.js
+++ app.js
@@ -14,15 +14,8 @@ app.get('/login', (req, res) => {
     res.send(`<h1>You are successfully logged in...</h1>`)
 })
 
-app.get('/urvilwedsdurva', (req, res) => {
-    res.send(`<h1>Dear Urvil,</h1>
-<p>Congratulations to you and Durva on your wedding! 💍❤️</p>
-<p>I'm truly happy to see you both starting this beautiful new chapter of your lives together.</p>
-<p>Wishing you a lifetime filled with love, happiness, understanding, endless laughter, and unforgettable memories.</p>
-<p>May your bond grow stronger with every passing day, and may you both always support and inspire each other through every phase of life.</p>
-<p>I'm so proud of you, brother.</p>
-<p>Wishing you and Durva a joyful, successful, and blessed married life ahead.</p>
-<p>Congratulations once again! 🎉🥂❤️</p>`)
+app.get('/urvil', (req, res) => {
+    res.send(`<h1>Dear Urvil, how are you??</h1>`)
 })
 
 app.listen(port, () => {
